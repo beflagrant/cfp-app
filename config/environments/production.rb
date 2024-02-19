5790d2f79f7cfb24fa763e69b6899c0f48cfd417
Cody Brooks <codyjamesbrooks@gmail.com>
2024-02-18 18:43:48 -0700

    Configure mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,8 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
   config.action_mailer.default_options = { from: ENV['MAIL_FROM'] }
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     address: ENV.fetch('SMTP_ADDRESS', 'smtp.sendgrid.net'),
     port: ENV.fetch('SMTP_PORT', '587'),
